98f809cf52d1ef4744b01b9e35be1185bdac46df
zh <2821022192@qq.com>
2025-06-09 19:34:17 +0800

    乐观锁解决超卖问题业务开发

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -15,6 +24,48 @@ import org.springframework.stereotype.Service;
  * @since 2021-12-22
  */
 @Service
+@Transactional
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
 
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    @Override
+    public Result seckillVoucher(Long voucherId) {
+        //1.查询优惠券信息
+        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+        //2.判断秒杀是否开始
+        //2.1秒杀尚未开始返回异常
+        if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
+            return Result.fail("秒杀尚未开始");
+        }
+        //2.2秒杀已结束返回异常
+        if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
+            return Result.fail("秒杀已经结束");
+        }
+        //3.判断库存是否充足
+        if (voucher.getStock() < 1) {
+            //3.1库存不足返回异常
+            return Result.fail("库存不足！");
+        }
+        //3.2库存充足扣减库存
+        boolean success = seckillVoucherService.update()
+                .setSql("stock = stock - 1") //相当于set条件 set stock = stock - 1
+                .eq("voucher_id", voucherId) //相当于where条件 where id = ? and stock = ?
+                .eq("stock",0).update();
+        if (!success) {
+            return Result.fail("库存不足！");
+        }
+        //4.创建订单，返回订单id
+        VoucherOrder voucherOrder = new VoucherOrder();
+        long orderId = redisIdWorker.nextId("order");//订单id
+        voucherOrder.setId(orderId);
+        Long userId = UserHolder.getUser().getId();//用户id
+        voucherOrder.setUserId(userId);
+        voucherOrder.setVoucherId(voucherId);//代金券id
+        save(voucherOrder);
+        return Result.ok(orderId);
+    }
 }
