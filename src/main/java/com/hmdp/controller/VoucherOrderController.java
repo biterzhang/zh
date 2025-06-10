5d995c1d99752e1336f1f57a6f865b74cad975a1
zh <2821022192@qq.com>
2025-06-10 11:15:23 +0800

    回退

--- src/main/java/com/hmdp/controller/VoucherOrderController.java
+++ src/main/java/com/hmdp/controller/VoucherOrderController.java
@@ -2,8 +2,6 @@ package com.hmdp.controller;
 
 
 import com.hmdp.dto.Result;
-import com.hmdp.service.IVoucherOrderService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,10 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/voucher-order")
 public class VoucherOrderController {
-    @Autowired
-    private IVoucherOrderService iVoucherOrderService;
     @PostMapping("seckill/{id}")
     public Result seckillVoucher(@PathVariable("id") Long voucherId) {
-        return iVoucherOrderService.seckillVoucher(voucherId);
+        return Result.fail("功能未完成");
     }
 }
