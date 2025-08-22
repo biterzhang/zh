f948e6e64f3ca584f21b78b88d132742f2af907c
biterzhang <121670078+biterzhang@users.noreply.github.com>
2025-08-22 15:27:59 +0800

    Update BlogCommentsController.java
    撤销测试

--- src/main/java/com/hmdp/controller/BlogCommentsController.java
+++ src/main/java/com/hmdp/controller/BlogCommentsController.java
@@ -16,9 +16,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/blog-comments")
 public class BlogCommentsController {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("hello world1");
-        System.out.println("hello world12");
-    }
+
 }
